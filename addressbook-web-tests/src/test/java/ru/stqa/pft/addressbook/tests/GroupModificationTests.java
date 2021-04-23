3a47d267dfbfc6b99ff90b9631fd156327ad902e
ValeryiaSapeha <>
2021-04-23 19:13:15 +0300

    Создан тест GroupModification

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGropModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGropModification();
+    app.getGroupHelper().returnToGroupPage();
+    app.getSessionHelper().logout();
+
+  }
+}
