837145251f6d5ba32f568e0ae5e3caf903af39e6
ValeryiaSapeha <>
2021-05-03 23:20:24 +0300

    В классы GroupDeletion, GroupModification добавлены проверки GroupCount

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,8 +9,8 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-   int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData ("test1", null, null));
     int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after, before + 1);
