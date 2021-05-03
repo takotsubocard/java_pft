11d3934be71d248f941a884c24f328cea9c55a3b
ValeryiaSapeha <>
2021-05-03 23:13:24 +0300

    В класс GroupCreation добавлен метод GroupCount

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,11 +9,11 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
+   int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData ("test1", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
     app.getSessionHelper().logout();
   }
 
