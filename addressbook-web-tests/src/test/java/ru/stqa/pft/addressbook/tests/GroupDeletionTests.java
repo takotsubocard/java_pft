c3937a403ab81b5d9d13fdf526ef6c41216e1157
ValeryiaSapeha <>
2021-05-04 12:48:49 +0300

    Реализован поиск максимального id в GroupCreation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -23,6 +23,9 @@ public class GroupDeletionTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() - 1);
     before.remove(before.size() - 1);
     Assert.assertEquals(before, after);
+
+
+
     app.getSessionHelper().logout();
   }
 
