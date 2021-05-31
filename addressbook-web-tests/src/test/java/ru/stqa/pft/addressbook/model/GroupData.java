68ca6db4a495b2b35c77e45f41b0738c6d9c350c
ValeryiaSapeha <82082354+takotsubocard@users.noreply.github.com>
2021-05-31 22:04:25 +0300

    7.2. Использован ORM Hibernate

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,21 +1,36 @@
 package ru.stqa.pft.addressbook.model;
 
-import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
-
+@Entity
+@Table(name="group_list")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column(name="group_id")
   private int id = Integer.MAX_VALUE;
-  @Expose
+
+//  @Expose
+  @Column(name="group_name")
   private String name;
-  @Expose
+
+ // @Expose
+  @Column(name="group_header")
+  @Type(type = "text")
   private String header;
-  @Expose
+
+ // @Expose
+  @Column(name="group_footer")
+  @Type(type = "text")
   private String footer;
 
   public int getId() {
